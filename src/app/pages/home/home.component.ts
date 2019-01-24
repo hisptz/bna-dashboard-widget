810479880f2508a939b71571478b18fa3f3dd3a5
ibrahim Wickama <ibrahimwickama@gmail.com>
2019-01-24 14:44:48 +0300

    update(home.component): update home component to default to current user orgunit & LASTYEAR period

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { State, getRouterParams } from '../../store';
+import {State, getRouterParams, getCurrentUser} from '../../store';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,9 +12,12 @@ export class HomeComponent implements OnInit {
   routerParams$: Observable<any>;
   selectedOrgUnit$: Observable<string>;
   selectedPeriod$: Observable<string>;
+  currentUser$: Observable<any>;
+  LastYear: any = new Date().getFullYear() - 1; // its hack for getting lastYear on init
 
   constructor(private store: Store<State>) {
     this.routerParams$ = store.select(getRouterParams);
+    this.currentUser$ = store.select(getCurrentUser);
   }
 
   ngOnInit() {}
